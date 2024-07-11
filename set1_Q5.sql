06de3b843fdcdfdb86c6541956c037372a8302a1
Aditi sahu <aditisahu.1405@gmail.com>
2024-07-11 23:16:26 +0530

    Add files via upload

---
+++ set1_Q5.sql
@@ -0,0 +1,11 @@
+--S1:Q5
+-- who is the best customer ? the customer who has spent the most
+-- money will be declared the best customer. write the query that returns
+--the person who spent the most money
+select customer.customer_id ,customer.first_name, customer.last_name, sum(total) as money_spend
+from invoice
+left join customer
+on customer.customer_id = invoice.customer_id
+group by customer.customer_id
+order by money_spend desc
+limit 1
